9ae0b8ecd0afb93d1ff95d621cf6c0019bbe8bd7
liuxinxin <cliuxinxin@gmail.com>
2023-04-04 15:38:51 +0800

    修改增加chatgpt支持

--- main-local.py
+++ main-local.py
@@ -4,9 +4,9 @@ from PyPDF2 import PdfReader
 import pandas as pd
 from openai.embeddings_utils import get_embedding, cosine_similarity
 import openai
-import os
 import requests
 from flask_cors import CORS
+from apikey import OPENAI_API_KEY
 
 app = Flask(__name__)
 CORS(app)
@@ -15,9 +15,9 @@ CORS(app)
 class Chatbot():
     
     def parse_paper(self, pdf):
-        print("Parsing paper")
+        print("解析论文")
         number_of_pages = len(pdf.pages)
-        print(f"Total number of pages: {number_of_pages}")
+        print(f"总页数: {number_of_pages}")
         paper_text = []
         for i in range(number_of_pages):
             page = pdf.pages[i]
@@ -26,7 +26,7 @@ class Chatbot():
             def visitor_body(text, cm, tm, fontDict, fontSize):
                 x = tm[4]
                 y = tm[5]
-                # ignore header/footer
+                # 忽略页眉页脚
                 if (y > 50 and y < 720) and (len(text.strip()) > 1):
                     page_text.append({
                     'fontsize': fontSize,
@@ -62,32 +62,29 @@ class Chatbot():
                     blob_font_size = t['fontsize']
                     blob_text = t['text']
                 paper_text += processed_text
-        print("Done parsing paper")
-        # print(paper_text)
+        print("完成解析论文")
         return paper_text
 
     def paper_df(self, pdf):
-        print('Creating dataframe')
+        print('创建数据框')
         filtered_pdf= []
         for row in pdf:
             if len(row['text']) < 30:
                 continue
             filtered_pdf.append(row)
         df = pd.DataFrame(filtered_pdf)
-        # print(df.shape)
-        # remove elements with identical df[text] and df[page] values
         df = df.drop_duplicates(subset=['text', 'page'], keep='first')
         df['length'] = df['text'].apply(lambda x: len(x))
-        print('Done creating dataframe')
+        print('完成创建数据框')
         return df
 
     def calculate_embeddings(self, df):
-        print('Calculating embeddings')
-        openai.api_key = os.getenv('OPENAI_API_KEY')
+        print('计算嵌入')
+        openai.api_key = OPENAI_API_KEY
         embedding_model = "text-embedding-ada-002"
         embeddings = df.text.apply([lambda x: get_embedding(x, engine=embedding_model)])
         df["embeddings"] = embeddings
-        print('Done calculating embeddings')
+        print('完成计算嵌入')
         return df
 
     def search_embeddings(self, df, query, n=3, pprint=True):
@@ -98,42 +95,50 @@ class Chatbot():
         df["similarity"] = df.embeddings.apply(lambda x: cosine_similarity(x, query_embedding))
         
         results = df.sort_values("similarity", ascending=False, ignore_index=True)
-        # make a dictionary of the the first three results with the page number as the key and the text as the value. The page number is a column in the dataframe.
         results = results.head(n)
         global sources 
         sources = []
         for i in range(n):
-            # append the page number and the text as a dict to the sources list
-            sources.append({'Page '+str(results.iloc[i]['page']): results.iloc[i]['text'][:150]+'...'})
+            sources.append({'第'+str(results.iloc[i]['page'])+'页': results.iloc[i]['text'][:150]+'...'})
         print(sources)
         return results.head(n)
-    
+
+
     def create_prompt(self, df, user_input):
         result = self.search_embeddings(df, user_input, n=3)
         print(result)
-        prompt = """You are a large language model whose expertise is reading and summarizing scientific papers. 
-        You are given a query and a series of text embeddings from a paper in order of their cosine similarity to the query.
-        You must take the given embeddings and return a very detailed summary of the paper that answers the query.
+        prompt = """你是一个专门阅读和总结科学论文的大型语言模型。 
+        你需要根据提出的问题和一系列与问题相似的论文中的文本嵌入来回答问题。
+        请根据给定的嵌入，返回一篇详细的论文摘要来回答问题。
             
-            Given the question: """+ user_input + """
+            问题：""" + user_input + """
             
-            and the following embeddings as data: 
+            以下是数据中的文本嵌入：
             
             1.""" + str(result.iloc[0]['text']) + """
             2.""" + str(result.iloc[1]['text']) + """
             3.""" + str(result.iloc[2]['text']) + """
 
-            Return a detailed answer based on the paper:"""
+            请根据论文返回一个详细的回答:"""
 
-        print('Done creating prompt')
+        print('完成创建提示')
         return prompt
 
     def gpt(self, prompt):
-        print('Sending request to GPT-3')
-        openai.api_key = os.getenv('OPENAI_API_KEY')
-        r = openai.Completion.create(model="text-davinci-003", prompt=prompt, temperature=0.4, max_tokens=1500)
-        answer = r.choices[0]['text']
-        print('Done sending request to GPT-3')
+        print('发送请求到 GPT-3')
+        openai.api_key = OPENAI_API_KEY
+    
+        messages = [{"role": "system", "content": prompt}]
+    
+        r = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages,
+            temperature=0.4,
+            max_tokens=1500
+        )
+    
+        answer = r.choices[0].message.content.strip()
+        print('完成发送请求到 GPT-3')
         response = {'answer': answer, 'sources': sources}
         return response
 
@@ -141,14 +146,14 @@ class Chatbot():
         print(prompt)
         prompt = self.create_prompt(df, prompt)
         return self.gpt(prompt)
-
+    
 @app.route("/", methods=["GET", "POST"])
 def index():
     return render_template("index.html")
 
 @app.route("/process_pdf", methods=['POST'])
 def process_pdf():
-    print("Processing pdf")
+    print("处理 PDF")
     file = request.data
     pdf = PdfReader(BytesIO(file))
     chatbot = Chatbot()
@@ -156,7 +161,7 @@ def process_pdf():
     global df
     df = chatbot.paper_df(paper_text)
     df = chatbot.calculate_embeddings(df)
-    print("Done processing pdf")
+    print("完成处理 PDF")
     return {'key': ''}
 
 @app.route("/download_pdf", methods=['POST'])
@@ -170,7 +175,7 @@ def download_pdf():
     global df
     df = chatbot.paper_df(paper_text)
     df = chatbot.calculate_embeddings(df)
-    print("Done processing pdf")
+    print("完成处理 PDF")
     return {'key': ''}
 
 @app.route("/reply", methods=['POST'])
